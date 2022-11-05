da6f9d893ca29ace7bbf3ad838d2e2ae92b8f29e
Dawin Valenzuela <dawin.valenzuela@gmail.com>
2022-11-05 08:52:58 -0500

    add InputWithLabel component with ts and react-hook-form

--- src/components/Suppliers/Suppliers.tsx
+++ src/components/Suppliers/Suppliers.tsx
@@ -1,5 +1,35 @@
+import { Button, Heading, Stack, VStack } from '@chakra-ui/react';
 import React from 'react';
+import { InputWithLabel } from 'src/components';
+import { useForm } from 'react-hook-form';
 
 export const Suppliers = () => {
-  return <div>Suppliers</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const onSubmit = () => {};
+
+  return (
+    <VStack mt={7} align='stretch' px={4} spacing={5}>
+      <Heading textAlign='center'>Crear proveedor</Heading>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={5}>
+          <InputWithLabel
+            formLabel='Nombre proveedor'
+            placeholder='Escriba nombre del proveedor'
+            inputName='name'
+            register={register}
+            rules={{ required: 'El nombre del proveedor es requerido' }}
+            errors={errors}
+          />
+          <Button colorScheme='blue' type='submit'>
+            Registrar
+          </Button>
+        </Stack>
+      </form>
+    </VStack>
+  );
 };
