e1a7943b7b56052ae543b7edb83d68f13281b310
Dawin Valenzuela <dawin.valenzuela@gmail.com>
2023-08-23 18:08:50 -0500

    protect routes

--- pages/services/barber-service/index.tsx
+++ pages/services/barber-service/index.tsx
@@ -1,3 +1,6 @@
+import { GetServerSidePropsContext } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from 'pages/api/auth/[...nextauth]';
 import { AddBarberService } from 'src/components';
 
 function AddService() {
@@ -5,3 +8,17 @@ function AddService() {
 }
 
 export default AddService;
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (!session) {
+    return {
+      redirect: { destination: '/login' },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
