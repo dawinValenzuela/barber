5b32545fed8ffe197386097e603bbc6f2c8ad8f3
Dawin Valenzuela <dawin.valenzuela@gmail.com>
2023-08-29 16:31:51 -0500

    test

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -10,8 +10,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(401).json({ error: 'Unauthorized' });
   }
 
-  console.log('request.auth:', session?.user);
-
   try {
     if (req.method === 'GET') {
       const allUsers = [];
